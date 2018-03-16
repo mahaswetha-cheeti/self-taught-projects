00a650c1bbe07fb00180805ef005f87b4fc40676
MahaSwetha <mahaswetha.svs@gmail.com>
2018-03-16 15:34:21 +1100

    my first git commit

---
+++ simple-maven-project/src/main/java/com/mytutorial/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.mytutorial;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("My first java code with eclipse IDE");
+		System.out.println("My first Git project");
+	}
+
+}
